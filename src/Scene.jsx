870f212fa438f290c35b4e8e3a156563e8956235
Guilherme <119014317+Guilhermezzq@users.noreply.github.com>
2024-05-05 14:27:25 -0300

    first commit

---
+++ src/Scene.jsx
@@ -0,0 +1,25 @@
+import { OrbitControls, PositionalAudio } from "@react-three/drei";
+import { useState } from "react";
+
+const Scene = () => {
+  const [play, setPlay] = useState(false);
+
+  const clickHandler = () => {
+    setPlay(!play);
+  };
+
+  return (
+    <>
+      <OrbitControls />
+
+      {play && <PositionalAudio url="./sound/sound.mp3" autoplay loop distance={4} /> }
+
+      <mesh onClick={clickHandler}>
+        <boxGeometry />
+        <meshBasicMaterial color="purple" />
+      </mesh>
+    </>
+  );
+};
+
+export default Scene;
